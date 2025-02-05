e038de7ac03af0e0a6dfba466e08b8134ef1f445
Olopper <olopper2407@g.educaand.es>
2025-02-05 14:10:52 +0100

    Exercise 3

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,5 +1,10 @@
 def fibonacci(n):
-    res = 
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
-fibonacci(6)
+print(fibonacci(6))
