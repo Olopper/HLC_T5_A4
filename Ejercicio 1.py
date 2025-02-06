39382cc4f26bf2dcbf42491c9fcc2fe7a8387f77
Olopper <olopper2407@g.educaand.es>
2025-02-06 09:54:10 +0100

    Exercise 1 corrected

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -3,6 +3,12 @@ def factorial_it(n):
     for i in range(1, n+1):
         res *= i
     return res
+
+def factorial_rec(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_rec(n-1)
  
 print(factorial_it(5))
-factorial_it(5)
+print(factorial_rec(5))
